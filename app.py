0ca449cf0b46a0f10f291b7aa787b278be0a492e
NhlalalaDylan <Nhlalalamaluleke332@gmail.com>
2021-03-18 20:30:50 +0200

    flaskIntro

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, url_for, render_template, redirect, request, session
+from markupsafe import escape
+from flask import request
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "sumo"
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+
+@app.route('/')
+def home():
+    # check if we reached page by Get or Post
+    return render_template('index.html')
+
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form['nm']
+        session["user"] = user
+        return redirect(url_for('user'))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template('login.html')
+
+
+@app.route('/user')
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1"
+    else:
+
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
